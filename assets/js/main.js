96c1dae6b798272c722607fa39b6d9da055da42f
jlgfmhldf <jlgfmhldf@gmail.com@>
2015-01-20 21:21:05 +0300

    + range slider

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#slider").jRange({
+    from: 0,
+    to: 100,
+    step: 1,
+    width: 760,
+    showSkale: false,
+    showLabels: false,
+    theme: "theme-csssr"
+})
+})
+    
